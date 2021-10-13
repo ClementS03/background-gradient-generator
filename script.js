85f0af89dc92b699b7ea002009066a524a6e75ee
ClementS03 <clement.seguin63@gmail.com>
2021-10-13 15:28:30 +0200

    simple background gradient generator

---
+++ script.js
@@ -0,0 +1,45 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var btn = document.querySelector(".random");
+
+// TODO
+// 3. Un boutton qui genere un gradient aleatoire
+
+function setGradientOnLoading() {
+    css = setGradient();
+}
+
+function generateRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color = color + letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function setRandomColor() {
+    color1.value = generateRandomColor();
+    color2.value = generateRandomColor();
+    body.style.background = setGradient();
+}
+
+function setGradient() {
+    body.style.background =
+        "linear-gradient(to right, "
+        + color1.value
+        + ", "
+        + color2.value
+        + ")";
+
+    css.textContent = body.style.background
+        + ';';
+
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+window.addEventListener("load", setGradientOnLoading)
+btn.addEventListener("click", setRandomColor)
